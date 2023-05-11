cfdad665a25077dfa9fd0c65d8670cb8302982c2
Ilham Bachtiar Irfani <irfanswag94@gmail.com>
2023-05-11 09:04:19 +0700

    [FIX] add migration

---
+++ backend/sequelize/migrations/20230511015449-create_bookmarks_table.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookmarks', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageTitle: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      thumbnail: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      screenshot: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      favicon: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      locale: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    }, {
+      paranoid: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookmarks');
+  }
+};
